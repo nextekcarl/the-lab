e738507249c6c81d76c805bba102e377d1acb7d2
Carl Anderson <nexusrpg@gmail.com>
2016-10-15 12:06:45 -0700

    Added training degrees to Superbeings

--- app/models/superbeing.rb
+++ app/models/superbeing.rb
@@ -1,10 +1,11 @@
 class Superbeing < ApplicationRecord
-  attr_accessor :powers, :stats, :real_origin
+  attr_accessor :powers, :stats, :real_origin, :degrees
 
   after_initialize do
     self.origin = set_origin if origin.blank?
     set_powers
     set_stats
+    set_degrees
     self.name = Faker::Superhero.name
   end
 
@@ -20,5 +21,14 @@ class Superbeing < ApplicationRecord
 
   def set_stats
     @stats ||= self.real_origin.roll_stats
+  end
+
+  def set_degrees
+    return @degrees unless @degrees.blank?
+    @degrees = []
+    (@stats[:intelligence]/7).times do
+      @degrees << Faker::Educator.course
+    end
+    @degrees = ["No advanced degrees of note"] if @degrees.empty?
   end
 end
